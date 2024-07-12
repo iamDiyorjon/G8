c89651c7c6a9f942b0513e16dbc57e7878bd7540
Javoxir Toshpo'latov <javohirtoshpulatov1303@gmail.com>
2024-07-12 21:53:21 +0500

    add 2 line

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,6 +21,7 @@ public static class Program
             Console.WriteLine("7. Attach Specialist to Teacher");
             Console.WriteLine("8. List Specialists");
             Console.WriteLine("9. List Teachers");
+
             Console.WriteLine("10. Get Full List");
             Console.WriteLine("11. Exit");
             Console.Write("Choose an option: ");
