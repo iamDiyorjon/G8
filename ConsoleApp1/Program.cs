5638fb6b86d4aadd63a6d94f11b45da1d975bd3a
Sadriddin01 <sadriddinuzakov1@icloud.com>
2024-07-12 21:54:54 +0300

    add new line with word

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,9 @@ public static class Program
 
         while (!exit)
         {
+            Console.WriteLine("----Hello tashkent----");
             Console.WriteLine("1. Add Specialist");
             Console.WriteLine("2. Add Teacher");
-
             Console.WriteLine("3. Update Specialist");
             Console.WriteLine("4. Update Teacher");
             Console.WriteLine("5. Delete Specialist");
